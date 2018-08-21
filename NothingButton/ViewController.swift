dbdad746a32ec5ab8ace09cf88d21b8421161bb4
Michael Linker <mlinker2231@jh-mba158-08.d214.org>
2018-08-21 12:11:49 -0500

    Initial Commit

---
+++ NothingButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NothingButton
+//
+//  Created by Michael Linker on 8/21/18.
+//  Copyright © 2018 John Hersey Highschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
